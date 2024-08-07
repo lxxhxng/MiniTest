3b9eae81eda6a3157c960df3facbabaafdad8ce9
Hyeonjun Park <baghyeonjun16@gmail.com>
2024-08-07 15:54:57 +0900

    PHJ_디자인 수정 및 필터 추가

--- src/main/java/org/example/miniproject_5/controller/ExamRegController.java
+++ src/main/java/org/example/miniproject_5/controller/ExamRegController.java
@@ -31,6 +31,7 @@ public class ExamRegController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         log.info("examregister GET ");
 
         req.getRequestDispatcher("/WEB-INF/teacher/examReg.jsp").forward(req, resp);
