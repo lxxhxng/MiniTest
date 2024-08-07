6b509606475a01b7587d8d72bce32a69b031a3d4
Hyeonjun Park <baghyeonjun16@gmail.com>
2024-08-07 14:42:56 +0900

    PHJ_logout기능 추가

---
+++ src/main/java/org/example/miniproject_5/controller/TLogoutController.java
@@ -0,0 +1,29 @@
+package org.example.miniproject_5.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.log4j.Log4j2;
+
+import java.io.IOException;
+
+@WebServlet(value = "/tlogout")
+@Log4j2
+public class TLogoutController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session = req.getSession(false);
+
+        if (session != null) {
+            // 세션에 'teacher' 속성이 있는 경우 삭제합니다.
+            session.removeAttribute("teacher");
+        }
+
+        resp.sendRedirect("/teacher/login");
+    }
+}
